3f4b9702ab08f1b735e75002924f16317754d8a5
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-07 10:45:45 +0100

    Added second effect hook , using Mounting and Updating phases

--- components-deep-dive/src/components/Book.js
+++ components-deep-dive/src/components/Book.js
@@ -1,27 +1,41 @@
-import {useState} from "react";
-import {useEffect} from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const Book = (props) => {
-    const [highlighted, setHighlighted] = useState(false)
-    const clickHandler =()=>{
-         setHighlighted(state=> !state);
+  const [highlighted, setHighlighted] = useState(false);
+  const [marked, setMarked] = useState(false);
 
-    }
-    useEffect(()=>{
-        console.log("Mounting")
-    }, [])
+  const clickHandler = () => {
+    setHighlighted((state) => !state);
+  };
+  const deleteHandler = () => {
+    setMarked (true);
+  };
 
-    let style= {};
-    if(highlighted){
-        style.background = "white";
-        style.color= "black"
-    }
+  useEffect(() => {
+    console.log("Mounting: " + props.title);
+  }, []);
+  useEffect(() => {
+    console.log("Update: " + props.title);
+  }, [highlighted]);
+
+  let style = {};
+  if (highlighted) {
+    style.background = "white";
+    style.color = "black";
+  }
+
+  if (marked) {
+    style.background = "green";
+  }
   return (
-    <li onClick={clickHandler} style={style}>
+    <li style={style}>
       <article>
         <h3>Title: {props.title}</h3>
         <p>Description: {props.description}</p>
         <footer>
+            <button onClick={clickHandler}>Highlight</button>
+            <button onClick={deleteHandler}>Delete</button>
           <span>Author: {props.author}</span>
         </footer>
       </article>
