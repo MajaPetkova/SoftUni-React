59fa514a159cca9e63a4e54d0809146ba652c6e5
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-09 19:09:05 +0100

    Added Search and UserSection components

--- user-list-workshop/client/src/App.js
+++ user-list-workshop/client/src/App.js
@@ -1,13 +1,19 @@
 import { Header } from "./components/common/Header";
 import "./App.css" ;
 import { Footer } from "./components/common/Footer";
+import { Search } from "./search/Search";
+import { UserSection } from "./user-section/UserSection";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-
-        
+        <main className="main">
+        <section className="card users-container">
+         <Search/>
+         <UserSection/>
+        </section>
+        </main>
         <Footer/>
       </header>
     </div>
