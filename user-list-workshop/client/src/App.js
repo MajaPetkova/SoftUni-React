1a6f76e44a736c2cd5db7093f4522df0f0028042
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-10 08:48:28 +0100

    fetching data from data base and display data in user item

--- user-list-workshop/client/src/App.js
+++ user-list-workshop/client/src/App.js
@@ -1,9 +1,20 @@
-import { Header } from "./components/common/Header";
+import { useEffect, useState } from "react";
 import "./App.css" ;
+import { Header } from "./components/common/Header";
 import { Footer } from "./components/common/Footer";
 import { Search } from "./search/Search";
-import { UserSection } from "./user-section/UserSection";
+import { UserList } from "./user-list/UserList";
+
+const baseUrl= "http://localhost:3005/api";
 function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+       fetch(`${baseUrl}/users`)
+       .then(res =>res.json())
+       .then(data => setUsers(data.users))
+    }, [])
+    console.log(users)
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +22,7 @@ function App() {
         <main className="main">
         <section className="card users-container">
          <Search/>
-         <UserSection/>
+         <UserList users={users}/>
         </section>
         </main>
         <Footer/>
