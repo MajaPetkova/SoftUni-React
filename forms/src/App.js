e55f4f32514a38f8a9875dd005de144cef32f5cc
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-13 18:51:21 +0100

    Update controlled form with one state for all input fields

--- forms/src/App.js
+++ forms/src/App.js
@@ -2,23 +2,44 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [age, setAge] = useState("");
-  const [bio, setBio] = useState("");
-  const [gender, setGender] = useState("f");
-  const [userType, setUserType] = useState("");
-  const [tac, setUserTac] = useState(false);
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+    age: "",
+    bio: "",
+    gender: "f",
+    userType: "individual",
+    tac: true,
+  });
+  //   const [username, setUsername] = useState("");
+  //   const [password, setPassword] = useState("");
+  //   const [age, setAge] = useState("");
+  //   const [bio, setBio] = useState("");
+  //  const [gender, setGender] = useState("f");
+  //  const [userType, setUserType] = useState("corporate");
+  //  const [tac, setUserTac] = useState(false);
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    let values= Object.fromEntries(new FormData(e.target))
-    console.log(values)
+  const changeHandler = (e) => {
+    setValues((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  
+  const checkboxChangeHandler = (e) => {
+    setValues((state) => ({
+        ...state,
+        [e.target.name]:!state[e.target.name] ,
+      }));
   };
 
-  const onChangeHandler = (e) => {
-    setUsername(e.target.value);
-  
+  //   const onChangeHandler = (e) => {
+  //     setUsername(e.target.value);
+  //   };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let values = Object.fromEntries(new FormData(e.target));
+    console.log(values);
   };
 
   return (
@@ -31,48 +52,82 @@ function App() {
               type="text"
               id="username"
               name="username"
-              onChange={onChangeHandler}
-              value={username}
+              onChange={changeHandler}
+              value={values.username}
             ></input>
           </div>
           <div>
             <label htmlFor="password">Password: </label>
-            <input type="password" id="password" name="password" value={password} onChange ={(e)=>setPassword(e.target.value)}></input>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={values.password}
+              onChange={changeHandler}
+            ></input>
           </div>
           <div>
             <label htmlFor="age">Age: </label>
-            <input type="number" id="age" name="age" value={age} onChange ={(e)=>setAge(Number(e.target.value))}></input>
+            <input
+              type="number"
+              id="age"
+              name="age"
+              value={values.age}
+              onChange={changeHandler}
+            ></input>
           </div>
           <div>
             <label htmlFor="bio">Bio: </label>
-            <textarea name="bio" id="bio" cols="30" rows="7" value={bio} onChange ={(e)=>setBio(e.target.value)}/>
+            <textarea
+              name="bio"
+              id="bio"
+              cols="30"
+              rows="7"
+              value={values.bio}
+              onChange={changeHandler}
+            />
           </div>
           <div>
             <label htmlFor="gender">Gender: </label>
-            <select id="gender" name="gender" value={gender} onChange={(e)=>setGender(e.target.value)} >
+            <select
+              id="gender"
+              name="gender"
+              value={values.gender}
+              onChange={changeHandler}
+            >
               <option value="m">Male</option>
               <option value="f">Female</option>
             </select>
           </div>
-                   <div>
+          <div>
             <label htmlFor="individual-user-type">Individual</label>
             <input
+              onChange={changeHandler}
+              checked={values.userType == "individual"}
               type="radio"
-              id="userType"
-              name="individual"
+              id="individual-user-type"
+              name="userType"
               value="individual"
             />
             <label htmlFor="corporate-user-type">Corporate</label>
             <input
+              onChange={changeHandler}
+              checked={values.userType == "corporate"}
               type="radio"
-              id="userType"
-              name="corporate"
+              id="corporate-user-type"
+              name="userType"
               value="corporate"
             />
           </div>
           <div>
             <label htmlFor="tac">Terms and Conditions: </label>
-            <input type="checkbox" id="tac" name="tac" />
+            <input
+              type="checkbox"
+              id="tac"
+              name="tac"
+              checked={values.tac}
+              onChange={checkboxChangeHandler}
+            />
           </div>
           <input type="submit" value="Login" />
         </form>
