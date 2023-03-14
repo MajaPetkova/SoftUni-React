54d5e0b38dca5a1fbb05f6a46af04508715f4b9a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-14 12:41:48 +0100

    Added validation for the input fileds

--- user-list-workshop/client/src/user-list/user-create/UserCreate.js
+++ user-list-workshop/client/src/user-list/user-create/UserCreate.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export const UserCreate = ({ onClose, onUserCreate }) => {
+  const[errors, setErrors] = useState({})
   const [values, setValues] = useState({
     firstName: "",
     lastName: "",
@@ -27,6 +28,26 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
 
     onUserCreate(userData)
   };
+
+  const minLength=(e, bound)=>{
+     setErrors(state => ({
+        ...state,
+     [e.target.name]: values[e.target.name].length < bound
+     }))
+    
+  };
+
+  const isPositive =(e)=>{
+   let number= Number(e.target.value);
+ 
+    setErrors(state => ({
+        ...state,
+     [e.target.name]: number < 0
+     }))
+  }
+  
+  
+  
   return (
     <div className="overlay">
       <div className="backdrop" onClick={onClose}></div>
@@ -66,11 +87,14 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
                     type="text"
                     value={values.firstName}
                     onChange={changeHandler}
+                    onBlur={(e)=>minLength(e, 3)}
                   />
                 </div>
+                {errors.firstName &&
                 <p className="form-error">
                   First name should be at least 3 characters long!
                 </p>
+                 }
               </div>
               <div className="form-group">
                 <label htmlFor="lastName">Last name</label>
@@ -84,11 +108,14 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
                     type="text"
                     value={values.lastName}
                     onChange={changeHandler}
+                    onBlur={(e)=>minLength(e, 3)}
                   />
                 </div>
+                {errors.lastName && 
                 <p className="form-error">
                   Last name should be at least 3 characters long!
                 </p>
+                }
               </div>
             </div>
 
@@ -105,9 +132,12 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
                     type="text"
                     value={values.email}
                     onChange={changeHandler}
+                    onBlur={(e)=>minLength(e, 5)}
                   />
                 </div>
+                {errors.lastName &&
                 <p className="form-error">Email is not valid!</p>
+                }
               </div>
               <div className="form-group">
                 <label htmlFor="phoneNumber">Phone number</label>
@@ -121,9 +151,12 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
                     type="text"
                     value={values.phoneNumber}
                     onChange={changeHandler}
+                    onBlur={(e)=>minLength(e, 6)}
                   />
                 </div>
+                {errors.phoneNumber &&
                 <p className="form-error">Phone number is not valid!</p>
+                }
               </div>
             </div>
 
@@ -139,9 +172,12 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
                   type="text"
                   value={values.imageUrl}
                   onChange={changeHandler}
+                  onBlur={(e)=>minLength(e, 3)}
                 />
               </div>
+              {errors.imageUrl &&
               <p className="form-error">ImageUrl is not valid!</p>
+              }
             </div>
 
             <div className="form-row">
@@ -157,11 +193,14 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
                     type="text"
                     value={values.country}
                     onChange={changeHandler}
+                    onBlur={(e)=>minLength(e, 2)}
                   />
                 </div>
+                    {errors.country &&
                 <p className="form-error">
                   Country should be at least 2 characters long!
                 </p>
+                    }
               </div>
               <div className="form-group">
                 <label htmlFor="city">City</label>
@@ -175,11 +214,14 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
                     type="text"
                     value={values.city}
                     onChange={changeHandler}
+                    onBlur={(e)=>minLength(e, 3)}
                   />
                 </div>
+                {errors.city &&
                 <p className="form-error">
                   City should be at least 3 characters long!
                 </p>
+                }
               </div>
             </div>
 
@@ -196,11 +238,14 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
                     type="text"
                     value={values.street}
                     onChange={changeHandler}
+                    onBlur={(e)=>minLength(e, 3)}
                   />
                 </div>
+                {errors.street && 
                 <p className="form-error">
                   Street should be at least 3 characters long!
                 </p>
+                }
               </div>
               <div className="form-group">
                 <label htmlFor="streetNumber">Street number</label>
@@ -214,11 +259,14 @@ export const UserCreate = ({ onClose, onUserCreate }) => {
                     type="text"
                     value={values.streetNumber}
                     onChange={changeHandler}
+                    onBlur={isPositive}
                   />
                 </div>
+                {errors.streetNumber && 
                 <p className="form-error">
                   Street number should be a positive number!
                 </p>
+                }
               </div>
             </div>
             <div id="form-actions">
