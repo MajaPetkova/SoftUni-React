075b00f71ac568cdba864ee2a8e96424f98bf0bd
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-15 08:07:20 +0100

    fetching data from api and getting next item (next button)

--- routing/src/App.js
+++ routing/src/App.js
@@ -5,17 +5,20 @@ import { Pricing } from './components/Pricing';
 import {About} from "./components/About"
 import { Contacts } from './components/Contacts';
 import Navigation from './components/Navigation';
+import { Products } from './components/Products';
+
 
 function App() {
   return (
     <div className="App">
      <h1>Hello react router</h1>
      <Navigation/>
-     
+
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/pricing' element={<Pricing/>}/>
+      <Route path='/products/:productId' element={<Products/>}/>
       <Route path='/contacts' element={<Contacts/>}/>
       <Route path='/*' element={<h2>Not Found</h2>}/>
      </Routes>
---
+++ routing/src/components/Products.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+export const Products = () => {
+  // const params= useParams();
+  const [starship, setStarship] = useState({});
+  const { productId } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/starships/${productId}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setStarship(data);
+      });
+  }, [productId]);
+  const nextProductHandler = () => {
+    //  TODO: redirect to next product
+    navigate(`/products/${Number(productId) + 1}`)
+  };
+
+  return (
+    <>
+      <h2>Products Page</h2>
+      <h3>Product {productId} Specifications</h3>
+      <ul>
+        <li>Name: {starship.name}</li>
+        <li>Model: {starship.model}</li>
+        <li>Passengers: {starship.passengers}</li>
+      </ul>
+      <button onClick={nextProductHandler}>Next</button>
+    </>
+  );
+};
