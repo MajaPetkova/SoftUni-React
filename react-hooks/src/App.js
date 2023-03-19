8b7c96ec3b21d5b928a90c5629ed618ee7d2395b
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-19 13:17:46 +0100

    add json file with task to server and fetching data from server

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,32 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TaskList } from "./components/TaskList";
 import styles from "./App.module.css";
 import { CreateTask } from "./components/CreateTask";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { _id: 1, title: "first" },
-    { _id: 2, title: "second" },
-    { _id: 3, title: "third" },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3030/jsonstore/todos")
+      .then((res) => res.json())
+      .then((result) => setTasks(Object.values(result)));
+  },[]);
+
 
   const taskCreateHandler = (newTask) => {
     setTasks((state) => [
       ...state,
       {
-        _id: state[state.length - 1]._id + 1,
+        _id: state[state.length - 1]?._id + 1 || 1,
         title: newTask,
       },
     ]);
   };
-  const taskDeleteHandler =(taskId)=>{
-    setTasks(state => state.filter(x=> x._id !== taskId))
-  }
+  const taskDeleteHandler = (taskId) => {
+    // setTasks((state) => state.filter((x) => x._id !== taskId));
+    
+  };
   return (
     <div className={styles["site-wrapper"]}>
       <h1>TODO APP</h1>
       <main>
-        <TaskList tasks={tasks}  taskDeleteHandler={taskDeleteHandler}/>
+        <TaskList tasks={tasks} taskDeleteHandler={taskDeleteHandler} />
         <CreateTask taskCreateHandler={taskCreateHandler} />
       </main>
     </div>
--- react-hooks/src/components/TaskItem.js
+++ react-hooks/src/components/TaskItem.js
@@ -3,10 +3,12 @@ import { useEffect } from "react";
 export const TaskItem =({title, taskDeleteHandler, taskId})=>{
     useEffect(()=>{
         console.log("Mount");
+
         return ()=>{
             console.log("Unmount")
         }
-    }, [])
+    }, []);
+    
     return(
         <li>{title} 
           <button onClick={()=>taskDeleteHandler(taskId)}>x</button>
