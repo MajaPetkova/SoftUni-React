411fae6b9352b0e3c417d5ee94581aea407fb4a6
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-24 18:24:36 +0100

    prepare files and add first class component

--- workshop-advanced-techniques/src/App.js
+++ workshop-advanced-techniques/src/App.js
@@ -1,25 +1,22 @@
-
+import {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-    
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+      <main className="App-header">
+    <ul>
+      <li>Task</li>
+    </ul>
+       
+      </main>
     </div>
-  );
+    )
+  }
 }
+
+
+
 
 export default App;
